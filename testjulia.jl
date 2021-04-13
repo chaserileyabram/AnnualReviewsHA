eb794195632b5f2be136d5c6929035c6d6b7f6a8
Chase Abram <abram@uchicago.edu>
2021-04-13 14:52:37 -0500

    another batch

--- testjulia.jl
+++ testjulia.jl
@@ -1,9 +1,13 @@
 # Chase Abram
 # Used for testing how to run on Midway
 
-io = open(ENV["SLURM_ARRAY_TASK_ID"]*"test.txt", "w");
+task_id = string(ENV["SLURM_ARRAY_TASK_ID"])
 
-write(io, "Test on "*ENV["SLURM_ARRAY_TASK_ID"]);
+io = open(task_id*"test.txt", "w");
+
+write(io, "Test on "*task_id);
 
 close(io);
+
+println("Print statement for "*task_id)
 
