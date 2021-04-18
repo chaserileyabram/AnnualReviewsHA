f1750e3f72436a796a865e619dc0629183254aa8
Chase Abram <abram@uchicago.edu>
2021-04-17 22:16:16 -0500

    gonna try to get packages by running a batch script I guess

---
+++ get_pkgs.jl
@@ -0,0 +1,15 @@
+# Chase Abram
+# For importing packages
+
+import Pkg
+Pkg.add("Roots")
+Pkg.add("Optim")
+Pkg.add("Parameters")
+Pkg.add("Interpolations")
+Pkg.add("Random")
+Pkg.add("Plots")
+Pkg.add("Distributions")
+Pkg.add("Statistics")
+Pkg.add("DelimitedFiles")
+Pkg.add("LinearAlgebra")
+Pkg.add("QuadGK")
